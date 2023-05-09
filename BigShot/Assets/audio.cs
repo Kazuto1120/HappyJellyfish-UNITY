49dbff22dfbbc23d4c3e99719f2747fcca9ecfa8
Kazuto1120 <117380810+Kazuto1120@users.noreply.github.com>
2023-05-08 23:28:38 -0400

    audio slider

--- BigShot/Assets/audio.cs
+++ BigShot/Assets/audio.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class audio : MonoBehaviour
 {
+    public AudioSource audio1;
+    public GameObject slider;
     // Start is called before the first frame update
     private void Awake()
     {
+        
+        slider = GameObject.Find("Slider");
+        slider.GetComponent<Slider>().value = PlayerPrefs.GetFloat("volume",1f);
         DontDestroyOnLoad(transform.gameObject);
+        
+        audio1.volume = slider.GetComponent<Slider>().value;
     }
+    public void volume()
+    {
+        audio1.volume = slider.GetComponent<Slider>().value;
+        PlayerPrefs.SetFloat("volume", audio1.volume);
+    }
+
 }
